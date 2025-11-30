36b53b1b0a79a1e462bbceaabc3dd014f3fabac8
bickrosss <madinatekeeva2006@gmail.com>
2025-11-30 21:33:09 +0300

    Create pep563_example.py

---
+++ examples/pep563_example.py
@@ -0,0 +1,25 @@
+class Rectangle:
+    def __init__(self, width: int, height: int, color: Color):
+        self.width = width
+        self.height = height
+        self.color = color
+
+class Rectangle:
+    def __init__(self, width: int, height: int, color: "Color") -> None:
+        self.width = width
+        self.height = height
+        self.color = color
+
+from __future__ import annotations
+
+class Rectangle:
+    def __init__(self, width: int, height: int, color: Color):
+        self.width = width
+        self.height = height
+        self.color = color
+
+class Color:
+    def __init__(self, r: int, g: int, b: int):
+        self.r = r
+        self.g = g
+        self.b = b
