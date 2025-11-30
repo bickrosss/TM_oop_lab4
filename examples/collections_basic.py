5ed061a9aa4af0caf873363a14a059e0fa500f50
bickrosss <madinatekeeva2006@gmail.com>
2025-11-30 21:30:36 +0300

    Create collections_basic.py

---
+++ examples/collections_basic.py
@@ -0,0 +1,9 @@
+from typing import List, Dict, Tuple, Set
+
+numbers: List[int] = [1, 2, 3]
+grades: Dict[str, float] = {"math": 4.5, "physics": 5.0}
+record: Tuple[str, int] = ("John", 25)
+tags: Set[str] = {"python", "typing"}
+
+numbers: list[int] = [1, 2, 3]
+grades: dict[str, float] = {"math": 5.0}
