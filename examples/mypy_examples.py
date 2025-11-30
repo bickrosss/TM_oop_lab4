817818d9be9923e48c3cbfb4d05f07257058d04b
bickrosss <madinatekeeva2006@gmail.com>
2025-11-30 21:32:42 +0300

    Create mypy_examples.py

---
+++ examples/mypy_examples.py
@@ -0,0 +1,11 @@
+def sq_sum(a: int, b: int) -> int:
+    return a*2 + b*2
+
+print(sq_sum(3, 4))
+
+def sq_sum(a: int, b: int) -> int:
+    return a*2 + b*2
+
+print(sq_sum(3, "4"))
+
+result = process_data(data)
